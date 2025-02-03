6ff4a56cc27c19d090ab8e900c58f7598b0d7bac
A'nsi Lema <ansilema@gmail.com>
2025-02-03 13:46:22 +0100

    Add get_uploader_display tag for personalized user display
    Introduced a custom template tag, `get_uploader_display`, to show "Vous" when the uploader is the current user and the username otherwise. Updated relevant template sections to utilize the new tag for a more user-friendly experience.

--- reviews/templatetags/reviews_extras.py
+++ reviews/templatetags/reviews_extras.py
@@ -5,3 +5,10 @@ register = Library()
 @register.filter
 def model_type(value):
     return type(value).__name__
+
+@register.simple_tag(takes_context=True)
+def get_uploader_display(context, user):
+    if context['user'] == user:
+        return 'Vous'
+    else:
+        return user.username
